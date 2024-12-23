f4188632785144733380be80e9763c124ebad30d
Riku Koskinen <riku.i.koskinen@gmail.com>
2024-12-23 11:00:06 +0200

    Tehtävä: 1.4: kurssitiedot, step4, VALMIS

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -20,7 +20,6 @@ const Part = (props) => {
 
 const Content = (props) => {
   console.log('Content', props)
-  const juju = props
   return (
     <>
       <Part part={props.content[0]} />
@@ -37,7 +36,7 @@ const Total = (props) => {
   console.log('Total', props)
   return (
     <div>
-      Total umber of exercises {props.part}
+      Total umber of exercises {props.content[0].exercise + props.content[1].exercise + props.content[2].exercise}
     </div>
   )
 }
@@ -46,26 +45,28 @@ const Total = (props) => {
 const App = () => {
 
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercise: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercise: 7
-  }
-  const part3 = {
-    name: 'State fo component',
-    exercise: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercise: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercise: 7
+    },
+    {
+      name: 'State fo component',
+      exercise: 14
+    }
+  ]
 
   return (
     <>
       <Header course={course} />
 
-      <Content content={[part1, part2, part3]} />
+      <Content content={parts} />
 
-      <Total part={part1.exercise + part2.exercise + part3.exercise} />
+      <Total content={parts} />
     </ >
   )
 }
