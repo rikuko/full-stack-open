d10fa86acbdda1234d537334131d46ea64aa0671
Riku Koskinen <riku.i.koskinen@gmail.com>
2024-12-27 13:29:25 +0200

    Tehtävä: 1.8: unicafe, step3, VALMIS

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -18,26 +18,27 @@ const Button = ({ handleClick, text }) => {
   )
 }
 
-const Display = (props) => {
-  console.log(props)
+const Statistics = ({ good, neutral, bad }) => {
+  console.log(good, neutral, bad)
 
-  const pos = props.value[0] * 1
-  const neg = props.value[2] * -1
+  const pos = good * 1
+  const neg = bad * -1
+  const all = good + neutral + bad
 
   return (
     <div>
-      <p>Good {props.value[0]}
+      <p>Good {good}
         <br />
-        Neutral {props.value[1]}
+        Neutral {neutral}
         <br />
-        Bad {props.value[2]}
+        Bad {bad}
         <br />
         <br />
-        All {props.value[3]}
+        All {all}
         <br />
-        Average {(pos + neg) / props.value[3]}
+        Average {(pos + neg) / all}
         <br />
-        Positive {props.value[0] / props.value[3] * 100} %
+        Positive {good / all * 100} %
       </p>
     </div>
   )
@@ -48,22 +49,18 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [allClicks, setAllClicks] = useState(0)
 
   const handleGoodClick = () => {
     setGood(good + 1)
-    setAllClicks(allClicks + 1)
 
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
-    setAllClicks(allClicks + 1)
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
-    setAllClicks(allClicks + 1)
   }
 
   return (
@@ -76,7 +73,7 @@ const App = () => {
 
       <Header header={<h2>Statistics</h2>} />
 
-      <Display value={[good, neutral, bad, allClicks]} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
 
 
     </div>
