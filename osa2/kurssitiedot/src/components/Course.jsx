1fa1b54184a882a8c786f5650c13d353fe428011
Riku Koskinen <riku.i.koskinen@gmail.com>
2025-01-06 17:29:32 +0200

    Tehtävä: 2.4 ja 2.5: kurssitiedot, step9 ka erillinen moduuli, VALMIS

--- osa2/kurssitiedot/src/components/Course.jsx
+++ osa2/kurssitiedot/src/components/Course.jsx
@@ -1,14 +1,70 @@
-import Header from "./Header";
-import Content from "./Content"
+import { useState } from "react"
 
 
-const Course = ({ course }) => {
-    console.log('Kurssi tiedot: ', course)
+const Header = ({ header }) => {
+    console.log('Header name: ', header)
+    return (
+        <div key={header.id}>
+            {header}
+        </div>
+    )
+}
+
+const Part = ({ parts, name }) => {
+    console.log('Part props ', parts)
+    console.log('Part props ', name)
     return (
         <>
-            <Header id={course.id} header={course.name} />
-            <Content content={course.parts} />
+            <Header header={<h2>{name}</h2>} />
+            {parts.map(part =>
+                <li key={part.id}>
+                    {part.name + ' ' + part.exercises}
+                </li>
+            )}
         </>
+
+    )
+}
+
+const Counter = ({ courses }) => {
+    console.log('Counter props: ', courses)
+
+    const allParts = courses.map(course => course.parts)
+    console.log('All parts ', allParts)
+
+    const allInOne = [].concat(...allParts)
+    console.log('All in one ', allInOne)
+
+    const exercises = allInOne.map(exercise => exercise.exercises)
+    console.log('Exercises ', exercises)
+
+
+    const count = 0
+    const sum0fExercises = exercises.reduce((counter, value) => counter + value, count,)
+    console.log('Sum of exercises: ', sum0fExercises)
+
+
+    return (
+        <div>
+            <p><strong>Total of {sum0fExercises} exercises</strong></p>
+        </div>
+    )
+}
+
+const Course = ({ courses }) => {
+    console.log('Courses prop: ', courses)
+
+    return (
+        <div>
+            <Header header={<h1>Web development curriculum</h1>} />
+            {courses.map(course =>
+                <div key={course.id}>
+                    {<Part parts={course.parts} name={course.name} />}
+                </div>
+            )
+            }
+            <Counter courses={courses} />
+        </div>
     )
 }
 export default Course;
--- osa2/kurssitiedot/src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-const Header = ({ header, id }) => {
-    console.log('Header name: ', header)
-    console.log('Header id: ', id)
-    return (
-        <h1 key={id}>{header}</h1>
-    )
-}
-export default Header
