34dda680429162da9a9efbca2edc068afff2fa68
Riku Koskinen <riku.i.koskinen@gmail.com>
2025-01-14 16:47:11 +0200

    Tehtävä: 2.8: puhelinluettelo, step3, VALMIS

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -7,19 +7,23 @@ const App = () => {
   const [contacts, setContacts] = useState([
     {
       id: 1,
-      name: 'Arto Hellas'
+      name: 'Arto Hellas',
+      number: 123456
     }
   ])
 
   const [newContact, setNewContact] = useState('Add new contact')
   const [names, setNames] = useState([])
 
+  const [newNumber, setNewNumber] = useState('123456')
+
   const addContact = () => {
     event.preventDefault()
     console.log('Add-button clicked ')
     const newObject = {
       id: contacts.length + 1,
-      name: newContact
+      name: newContact,
+      number: newNumber
     }
 
     if (names.includes(newContact)) {
@@ -28,23 +32,27 @@ const App = () => {
     else setContacts(contacts.concat(newObject))
     setNewContact('')
     setNames([])
+    setNewNumber('')
   }
 
   const handleNewContact = (event) => {
     console.log('Handle new contact: ', event.target.value)
     setNewContact(event.target.value)
     setNames(names.concat(contacts.map(contact => contact.name)))
+  }
 
+  const handleNewNumber = (event) => {
+    console.log('Handle new number ', event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
     <div>
       <h2>Phone Book</h2>
       <form onSubmit={addContact}>
+        <div>Name: <input value={newContact} onChange={handleNewContact} /></div>
+        <div>Number: <input value={newNumber} onChange={handleNewNumber} /></div>
         <div>
-          Name: <input value={newContact}
-            onChange={handleNewContact}
-          />
           <button type="submit">Add</button>
         </div>
       </form>
