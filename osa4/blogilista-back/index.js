79a817c84082240484bdcebc5310d827762f7bf4
Riku Koskinen <riku.i.koskinen@gmail.com>
2025-03-28 16:55:20 +0200

    Tehtävä 4.1 blogilista, step1, VALMIS

---
+++ osa4/blogilista-back/index.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+const { config } = require('dotenv')
+const express = require('express')
+const app = express()
+/* const cors = require('cors') */
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://rikuikoskinen:DgvzPS4X8E5GFtru@cluster0.81st1.mongodb.net/blogList?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(mongoUrl)
+
+/* app.use(cors()) */
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/blogs', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
