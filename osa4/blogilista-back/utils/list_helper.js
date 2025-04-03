f242c488fd65e4e920b388c0d85b5e59adbcb3eb
Riku Koskinen <riku.i.koskinen@gmail.com>
2025-04-03 17:04:17 +0300

    Tehtävä 4.4: apufunktioita ja yksikkötestejä, step2, VALMIS

--- osa4/blogilista-back/utils/list_helper.js
+++ osa4/blogilista-back/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return blogs.length === 0 ? 1 : 0
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  }
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
