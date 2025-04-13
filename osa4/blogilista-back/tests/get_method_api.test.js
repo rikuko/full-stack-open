56317815d030849bb071b01cd6660ac24527c8a3
Riku Koskinen <riku.i.koskinen@gmail.com>
2025-04-13 13:52:48 +0300

    Tehtävä 4.9: blogilistan testit, step2, VALMIS

--- osa4/blogilista-back/tests/get_method_api.test.js
+++ osa4/blogilista-back/tests/get_method_api.test.js
@@ -6,32 +6,36 @@ const app = require('../app')
 
 const api = supertest(app)
 
-describe('Blog API tests', () => {
+describe('GET-method API tests', () => {
 
-  test.only('all blogs are returned as JSON', async () => {
+  test('all blogs are returned as JSON', async () => {
     await api.get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
   })
 
-
-  //TODO: Testi kuntoon .Tehtävä 4.8
-  test.only('Two blogs in list', async () => {
+  test('Two blogs in list', async () => {
     const response = await api.get('/api/blogs')
     console.log('Blogs in DB: ',response.body)
     assert.strictEqual(response.body.length, 2)
   })
 
-  test.only('Not one blog in list', async () => {
+  test('Not one blog in list', async () => {
     const response = await api.get('/api/blogs')
     assert.notEqual(response.body.length, 1)
   })
 
-  test.only('Not three blog in list', async () => {
+  test('Not three blog in list', async () => {
     const response = await api.get('/api/blogs')
     assert.notEqual(response.body.length, 3)
   })
 
+  test('returned object id-field is named id', async () => {
+    const response = await api.get('/api/blogs')
+    console.log('Response: ',response.body)
+    assert.ok('id' in response.body[0])
+    assert.ok(!('_id' in response.body[0]))
+  })
 
   after(async () => {
     await mongoose.connection.close()
