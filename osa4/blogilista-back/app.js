683fafd49a16cdd7eb8cb5524ea5ec53e672d4b1
Riku Koskinen <riku.i.koskinen@gmail.com>
2025-05-14 16:41:54 +0300

    Tehtävä 4.18, blogilistan laajennus, step6, VALMIS

--- osa4/blogilista-back/app.js
+++ osa4/blogilista-back/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const app = express()
 const blogsRouter = require('./controllers/blogs_routes')
 const usersRouter = require('./controllers/users_routes')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 /* const cors = require('cors'); */
@@ -29,6 +30,7 @@ app.use(middleware.requestLogger)
 // Routes
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 
 // Middleware
