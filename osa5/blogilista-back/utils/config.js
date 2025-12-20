981b4042e64f640236221dd5d11d2aaacba05f34
Riku Koskinen <riku.i.koskinen@gmail.com>
2025-12-20 12:54:16 +0200

    Osan viisi aloitus

--- osa5/blogilista-back/utils/config.js
+++ osa5/blogilista-back/utils/config.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 const MONGODB_URI =
   process.env.NODE_ENV === 'test'
-    ? process.env.TEST_MONGODB_URI
-    : process.env.MONGODB_URI
+    ? process.env.TEST_MONGO_URI
+    : process.env.MONGO_URI
 
 const PORT = process.env.PORT || 3001
 
