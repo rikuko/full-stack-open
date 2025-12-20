ad886ec78c6e23268a3c0c8a57984be7d17f5a0c
Riku Koskinen <riku.i.koskinen@gmail.com>
2025-12-20 15:06:03 +0200

    Kirjautumislomake lisätty. Logikka puutuu

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -4,16 +4,53 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('Logging credentials: ', username, password)
+  }
+
+// ! TODO: Logiikan lisääminen kirjautumislomakkeelle 
 
   return (
     <div>
-      <h2>blogs</h2>
+
+      <h2>Login</h2>
+      <form onSubmit={{ handleLogin }}>
+        <div>
+          <label>
+            Username
+            <input
+              type='text'
+              value={username}
+              onChange={({ target }) =>
+                setUsername(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Password
+            <input
+              type='password'
+              value={password}
+              onChange={({ target }) =>
+                setPassword(target.value)}
+            />
+          </label>
+        </div>
+        <button type='submit'>Login</button>
+      </form>
+
+      <h2>Blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
