6ff2e860762eba5466197dd9f51c4d7a97f29866
Riku Koskinen <riku.i.koskinen@gmail.com>
2025-12-21 13:11:06 +0200

    Tehtävä 5.1: blogilistan frontent, step1, VALMIS

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -13,48 +15,62 @@ const App = () => {
     )
   }, [])
 
-  const handleLogin = (event) => {
+  const handleLogin = async event => {
     event.preventDefault()
-    console.log('Logging credentials: ', username, password)
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      console.log('Logging credentials: ', username, password)
+    } catch {
+      console.log('Wrong credentials: ', username, password)
+    }
   }
 
-// ! TODO: Logiikan lisääminen kirjautumislomakkeelle 
+  if (user === null) {
+    return (
+      <div>
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label>
+              Username
+              <input
+                type='text'
+                value={username}
+                onChange={({ target }) =>
+                  setUsername(target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Password
+              <input
+                type='password'
+                value={password}
+                onChange={({ target }) =>
+                  setPassword(target.value)}
+              />
+            </label>
+          </div>
+          <button type='submit'>Login</button>
+        </form>
+      </div>
+    )
+  }
 
   return (
     <div>
-
-      <h2>Login</h2>
-      <form onSubmit={{ handleLogin }}>
-        <div>
-          <label>
-            Username
-            <input
-              type='text'
-              value={username}
-              onChange={({ target }) =>
-                setUsername(target.value)}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password
-            <input
-              type='password'
-              value={password}
-              onChange={({ target }) =>
-                setPassword(target.value)}
-            />
-          </label>
-        </div>
-        <button type='submit'>Login</button>
-      </form>
-
       <h2>Blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+      <h4>Your are logged in as {user.name}</h4>
+      {
+        blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+    </div >
   )
 }
 
