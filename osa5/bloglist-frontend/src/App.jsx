2411ef52ed4c6a8f97ce12a2506c0212ed82f24d
Riku Koskinen <riku.i.koskinen@gmail.com>
2026-01-11 16:00:54 +0200

    Tehtävä 5.6 blogilistan frontend, step6 valmis

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Button from './components/Button'
 import Login from './components/Login'
 import Notification from './components/Notification'
 import BlogForm from './components/BlogForm'
+import Reusable from './components/Reusable'
 
 import blogService from './services/blogs'
 import loginService from './services/login'
@@ -105,27 +106,16 @@ const App = () => {
   }
 
   const loginForm = () => {
-    const hideLoginForm = { display: loginVisible ? 'none' : '' }
-    const showLoginForm = { display: loginVisible ? '' : 'none' }
-
     return (
-      <div>
-        <div style={hideLoginForm}>
-          <button onClick={() => setLoginVisible(true)}>Login</button>
-        </div>
-        <div>
-          <div style={showLoginForm}>
-            <Login
-              username={username}
-              password={password}
-              handleUsername={({ target }) => setUsername(target.value)}
-              handlePassword={({ target }) => setPassword(target.value)}
-              handleSubmit={handleLogin}
-            />
-            <button onClick={() => setLoginVisible(false)}>Cancel</button>
-          </div>
-        </div>
-      </div>
+      <Reusable buttonLabel="Login">
+        <Login
+          username={username}
+          password={password}
+          handleUsername={({ target }) => setUsername(target.value)}
+          handlePassword={({ target }) => setPassword(target.value)}
+          handleSubmit={handleLogin}
+        />
+      </Reusable>
     )
   }
 
@@ -145,44 +135,6 @@ const App = () => {
     )
   }
 
-  /*
-    if (user === null) {
-      return (
-        
-              <div>
-                <h2>Login</h2>
-                <Notification message={message} errorMessage={errorMessage} />
-                <form>
-                  <div>
-                    <label>
-                      Username
-                      <input
-                        type='text'
-                        value={username}
-                        onChange={({ target }) =>
-                          setUsername(target.value)}
-                      />
-                    </label>
-                  </div>
-                  <div>
-                    <label>
-                      Password
-                      <input
-                        type='password'
-                        value={password}
-                        onChange={({ target }) =>
-                          setPassword(target.value)}
-                      />
-                    </label>
-                  </div>
-                  <Button click={handleLogin} text='Login' />
-                </form>
-              </div>
-      
-      )
-    }
-    */
-
   return (
     <div>
       <h2>Blogs</h2>
@@ -193,56 +145,11 @@ const App = () => {
         <div>
           <h4>
             Your are logged in as {user.name}</h4>
-            {blogForm}
+          {blogForm()}
           <div>
-
             <Button click={handleLogout} text='Logout' />
             <p></p>
           </div>
-
-          {/*
-          <div>
-            <h3>Create new entry</h3>
-            <form>
-              <div>
-                <label>
-                  Title:
-                  <input
-                    type='text'
-                    value={title}
-                    onChange={({ target }) =>
-                      setTitle(target.value)}
-                  />
-                </label>
-              </div>
-              <div>
-                <label>
-                  Author:
-                  <input
-                    type='text'
-                    value={author}
-                    onChange={({ target }) =>
-                      setAuthor(target.value)}
-                  />
-                </label>
-              </div>
-              <div>
-                <label>
-                  Url:
-                  <input
-                    type='text'
-                    value={url}
-                    onChange={({ target }) =>
-                      setUrl(target.value)}
-                  />
-                </label>
-              </div>
-              <p></p>
-              <Button click={addNewBlog} text='Save' />
-              <p></p>
-            </form>
-          </div>
-*/}
 
           {
             blogs.map(blog =>
