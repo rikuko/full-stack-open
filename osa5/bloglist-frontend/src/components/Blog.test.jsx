785d7be3f4e79cdf8f8a0cf0196b8465260429bf
Riku Koskinen <riku.i.koskinen@gmail.com>
2026-01-31 16:37:25 +0200

    Tehtävä 5.15: blogilistan testit, step3, VALMIS

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import { expect } from 'vitest'
 
 const user = {
   username: 'testuser',
@@ -27,7 +28,7 @@ test('Renderöi blogin authorin', () => {
   expect(element).toBeDefined()
 })
 
-test('Renderöi blogin URLn', async () => {
+test('Renderöi blogin URLn jos kaikki blogin tiedot on klikattu näkyviin', async () => {
   render(<Blog blog={ blog } />)
   const user = userEvent.setup()
   const button = screen.getByText('Show')
@@ -35,6 +36,22 @@ test('Renderöi blogin URLn', async () => {
 
   const element = screen.getByText(/www.testi.fi/i)
   expect(element).toBeDefined()
+})
+
+test('Lisää kutsuu tapahtumakäsittelijää kahdesti kun like-nappia painetaan kahdesti', async () => {
+
+  const mockUpdate = vi.fn()
+
+  render(<Blog blog={ blog } updateBlog={ mockUpdate }/>)
+
+  const user = userEvent.setup()
+  const showButton = screen.getByText('Show')
+  await user.click(showButton)
+
+  const likeButton = screen.getByText('Like')
+  await user.click(likeButton)
+  await user.click(likeButton)
+  expect(mockUpdate).toBeCalledTimes(2)
 })
 
 
