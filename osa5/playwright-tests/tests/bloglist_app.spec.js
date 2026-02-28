82d0e48456b035a39bf4a76b01a10336a9655acb
Riku Koskinen <riku.i.koskinen@gmail.com>
2026-02-28 17:13:53 +0200

    Tehtävä 5.21: blogilistan end to end ‑testit, step5, VALMIS

--- osa5/playwright-tests/tests/bloglist_app.spec.js
+++ osa5/playwright-tests/tests/bloglist_app.spec.js
@@ -11,6 +11,13 @@ describe('Bloglist app tests', () => {
                 password: 'salainen'
             }
         })
+        await request.post('http://localhost:3001/api/users', {
+            data: {
+                name: 'Päivi Bloggaaja',
+                username: 'paivi',
+                password: 'salainen'
+            }
+        })
         await page.goto('/')
     })
 
@@ -36,23 +43,44 @@ describe('Bloglist app tests', () => {
     })
 
     describe('Processing blogs', () => {
-        beforeEach(async ({ page }) => {
+        test('Logged user can add new blog', async ({ page }) => {
             await loginWith(page, 'rkos', 'salainen')
             await addNewBlog(page, 'Test blog', 'Pete Blogger', 'www.google.com')
-        })
-
-        test('Logged user can add new blog', async ({ page }) => {
             const blogCont = page.locator('.blogCont')
             await expect(blogCont.getByRole('button', { name: 'Show' })).toBeVisible()
             await expect(blogCont).toContainText('Test blog')
         })
 
         test('Blogs can be liked', async ({ page }) => {
+            await loginWith(page, 'rkos', 'salainen')
+            await addNewBlog(page, 'Test blog', 'Pete Blogger', 'www.google.com')
             const blogCont = page.locator('.blogCont')
             await blogCont.getByRole('button', { name: 'Show' }).click()
             await expect(blogCont).toContainText('Likes 0')
             await blogCont.getByRole('button', { name: 'Like' }).click()
             await expect(blogCont).toContainText('Likes 1')
         })
+
+        test('The user who added the blog can delete the blog', async ({ page }) => {
+            await loginWith(page, 'rkos', 'salainen')
+            await addNewBlog(page, 'Test blog', 'Pete Blogger', 'www.google.com')
+            const blogCont = page.locator('.blogCont')
+            await blogCont.getByRole('button', { name: 'Show' }).click()
+
+            page.once('dialog', async dialog => {
+                expect(dialog.type()).toBe('confirm')
+                expect(dialog.message()).toBe(
+                    'Remove blog Test blog by Pete Blogger?'
+                )
+                await dialog.accept()
+            })
+
+            const blog = blogCont.getByText('Test blog')
+            await blog.locator('button:has-text("Remove")').click()
+
+            await expect(blog.getByText('Test blog')).not.toBeVisible()
+        })
+
     })
 })
+
