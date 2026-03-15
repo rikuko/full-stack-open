78a69e383cab75f1bb4e68244e30b148444bae5c
Riku Koskinen <riku.i.koskinen@gmail.com>
2026-03-15 14:46:09 +0200

    Tehtävä 6.8: anekdootit, step6, VALMIS

--- osa6/redux-anecdotes/src/App.jsx
+++ osa6/redux-anecdotes/src/App.jsx
@@ -1,30 +1,12 @@
-import { voteAnecdote } from './reducers/anecdoteReducer'
-import { useSelector, useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const dispatch = useDispatch()
-  const anecdotes = useSelector(state => [ ...state ].sort((a, b) => b.votes -a.votes))
-
-  const vote = id => {
-    console.log('vote', id)
-    dispatch(voteAnecdote(id))   
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      { anecdotes.map(anecdote => (
-        <div key = { anecdote.id }>
-          <div>{ anecdote.content }</div>
-          <div>
-            has { anecdote.votes }
-            <button onClick = { () => vote(anecdote.id) }>vote</button>
-          </div>
-        </div>
-      )) }
       <AnecdoteForm />
-      
+      <AnecdoteList />
     </div>
   )
 }
